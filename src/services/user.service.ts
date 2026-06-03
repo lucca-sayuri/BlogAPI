29d25ed62a6564e155b86a65d4a3144c2cb82e47
Lucca Yuki <kelcoolg@gmail.com>
2026-06-03 08:41:16 -0300

    Cadastro do usuário

---
+++ src/services/user.service.ts
@@ -0,0 +1,29 @@
+import { create } from 'node:domain'
+import { prisma } from '../libs/prisma'
+import bcrypt from 'bcryptjs'
+
+type CreateUserProps = {
+    name: string
+    email: string
+    password: string
+}
+
+export const createUser = async ({ name, email, password }: CreateUserProps ) => {
+    email = email.toLowerCase()
+
+    const user = await prisma.user.findFirst({
+        where: { email }
+    })
+
+    if (user) return false
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    return await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword
+        }
+    })
+}
